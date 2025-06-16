dc0b869ac481741c0443971a72e7e1c891cdc279
qnorris <129627426+qnorris@users.noreply.github.com>
2025-06-16 08:54:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if not st.session_state.authenticated:
         submitted = st.form_submit_button("Enter")
         
         if submitted:
-            if password == "102938":  # CHANGE THIS
+            if password == "Password":  # CHANGE THIS
                 st.session_state.authenticated = True
                 st.experimental_rerun()
             else:
